d2feb63ec32f914e9344721b08592f2055c8b836
Welley <monteirowelley7@gmail.com>
2024-08-08 12:38:28 -0300

    primeiro grafico gerado

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ import yfinance as yf
 def carregar_dados(empresa):
     dados_acao = yf.Ticker(empresa)
     contacao_acao = dados_acao.history(period='1d', start='2010-01-01', end='2024-07-01')
+    contacao_acao = contacao_acao[['Close']]
+    print(contacao_acao)
     return contacao_acao
 
 
 # Preparar as visualizações
 
-print(carregar_dados('ITUB4.SA'))
-
+dados = carregar_dados('ITUB4.SA')
 
 #mostrar graficos nas tela
 
@@ -22,6 +23,9 @@ st.write('''
     # Meu primeiro site em streamlit
     Desejo fazer graficos interativos.
 ''')
+
+# Criar os graficos
+st.line_chart(dados)
 
 st.write('''
     # Fim
