7c5b0036259f0e09c9c8b3a22667fb6e2aefbea5
Welley <monteirowelley7@gmail.com>
2024-08-09 10:38:56 -0300

    colocando o site no theme dark, e preparando para filtar

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import yfinance as yf
 
 # Criando as funçoes de carregamento de dados
 @st.cache_data
-def carregar_dados(empresa):
-    dados_acao = yf.Ticker(empresa)
+def carregar_dados(empresas):
+    text_tickets = ' '.join(empresas)
+    dados_acao = yf.Tickers(text_tickets)
     contacao_acao = dados_acao.history(period='1d', start='2010-01-01', end='2024-07-01')
-    contacao_acao = contacao_acao[['Close']]
-    print(contacao_acao)
+    contacao_acao = contacao_acao['Close']
     return contacao_acao
 
+acoes = ['ITUB4.SA','PETR4.SA','MGLU3.SA','VALE3.SA','ABEV3.SA','GGBR4.SA']
+dados = carregar_dados(acoes)
 
-# Preparar as visualizações
 
-dados = carregar_dados('ITUB4.SA')
 
 #mostrar graficos nas tela
 
@@ -24,8 +24,14 @@ st.write('''
     Desejo fazer graficos interativos.
 ''')
 
+# Preparar as visualizações
+
+
+
+
 # Criar os graficos
 st.line_chart(dados)
+
 
 st.write('''
     # Fim
